d8a9da3632f2f54ecb73c420790a4c1f28bcfca6
kostyapoymenov <kostya6699@mail.ru>
2018-06-21 21:34:38 +0300

    index page complete all

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,36 @@ $(document).ready(function(){
         loop: true,
         dots: true
     });
+
+    function burger(){
+		const   hamburgerBtn = document.querySelector('.hamburger'),
+				hamburgerItem = document.querySelectorAll('.hamburger-menu__link'),
+				hamburgerMenu = document.querySelector('.hamburger-menu');
+
+
+		hamburgerBtn.addEventListener('click', function(){
+			hamburgerBtn.classList.toggle('hamburger--active');
+			hamburgerMenu.classList.toggle('hamburger-menu--active');
+			if(hamburgerBtn.classList.contains('hamburger--active')){
+				document.body.classList.add('body-hidden');
+			} else {
+				document.body.classList.remove('body-hidden');
+			}
+		});
+
+		hamburgerMenu.addEventListener('click', function(event){
+			var target = event.target;
+
+			for(var i = 0; i < hamburgerItem.length; i++){
+				if(target == hamburgerItem[i]){
+					hamburgerBtn.classList.remove('hamburger--active');
+					hamburgerMenu.classList.remove('hamburger-menu--active');
+					document.body.classList.remove('body-hidden');
+					scroll(hamburgerItem[i]);
+				}
+			}
+
+		});
+	}
+    burger();
 });
