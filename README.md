cecdca7957b1134edc6d72a4a92dcbb042de6207
kostyapoymenov <kostya6699@mail.ru>
2018-10-11 20:39:33 +0300

    Create modal window Sign in and error message

---
+++ README.md
@@ -0,0 +1,10 @@
+Проект ROPIT
+
+Ссылка на gh-pages: https://kostyapoymenov.github.io/ropit/build/index.html
+
+How to install and build my app
+
+git clone https://github.com/kostyapoymenov/infobim
+cd ropit
+npm i
+npm run build
--- src/js/main.js
+++ src/js/main.js
@@ -4,72 +4,95 @@ $(document).ready(function(){
         items: 1,
         loop: true,
         dots: true
-    });
+		});
+		
+		// Модальные окна
+
+		function modal(content, btn){
+			$(document).on('click', btn, function(){
+					var template = $(content).html();
+					console.log(template);
+					$('body').append("<div class='modal__overlay'></div>");
+					$('body').css("overflow", "hidden");
+					$('.modal__overlay').append(template);
+
+					$(document).on('click', '.modal__close', function(){
+							//event.preventDefault();
+
+							$('.modal__overlay').remove();
+							$('.modal').remove();
+							$('body').css("overflow", "auto");
+					});
+			});
+		}
+
+		modal('#modalSignTemplate', '.btn-sign');	
 
 		// Бургер меню
     function burger(){
-		const   hamburgerBtn = document.querySelector('.hamburger'),
-						hamburgerItem = document.querySelectorAll('.hamburger-menu__link'),
-						hamburgerMenu = document.querySelector('.hamburger-menu');
+			const   hamburgerBtn = document.querySelector('.hamburger'),
+							hamburgerItem = document.querySelectorAll('.hamburger-menu__link'),
+							hamburgerMenu = document.querySelector('.hamburger-menu');
 
 
-		hamburgerBtn.addEventListener('click', function(){
-			hamburgerBtn.classList.toggle('hamburger--active');
-			hamburgerMenu.classList.toggle('hamburger-menu--active');
-			if(hamburgerBtn.classList.contains('hamburger--active')){
-				document.body.classList.add('body-hidden');
-			} else {
-				document.body.classList.remove('body-hidden');
-			}
-		});
-
-		hamburgerMenu.addEventListener('click', function(event){
-			var target = event.target;
-
-			for(var i = 0; i < hamburgerItem.length; i++){
-				if(target == hamburgerItem[i]){
-					hamburgerBtn.classList.remove('hamburger--active');
-					hamburgerMenu.classList.remove('hamburger-menu--active');
+			hamburgerBtn.addEventListener('click', function(){
+				hamburgerBtn.classList.toggle('hamburger--active');
+				hamburgerMenu.classList.toggle('hamburger-menu--active');
+				if(hamburgerBtn.classList.contains('hamburger--active')){
+					document.body.classList.add('body-hidden');
+				} else {
 					document.body.classList.remove('body-hidden');
-					scroll(hamburgerItem[i]);
+				}
+			});
+
+			hamburgerMenu.addEventListener('click', function(event){
+				var target = event.target;
+
+				for(var i = 0; i < hamburgerItem.length; i++){
+					if(target == hamburgerItem[i]){
+						hamburgerBtn.classList.remove('hamburger--active');
+						hamburgerMenu.classList.remove('hamburger-menu--active');
+						document.body.classList.remove('body-hidden');
+						scroll(hamburgerItem[i]);
+					}
+				}
+			});
+		}
+
+		burger();
+		
+		// Рассчет высоты формы регистрации и переходы
+		function formBlockHeight() {
+			var 	formBlock = document.querySelectorAll('.form-block'),
+						regSection = document.querySelector('.reg-section'),
+						btnNext = document.querySelector('.reg-bottom__next'),
+						btnBack = document.querySelector('.reg-bottom__back'),
+						regItemUser = document.querySelector('.reg-item--user'),
+						regItemCompany = document.querySelector('.reg-item--company'),
+						maxHeight = 0;
+		
+			for(var i = 0; i < formBlock.length; i++){
+				
+				var elemHeight = formBlock[i].offsetHeight;
+				if(elemHeight > maxHeight){
+					maxHeight = elemHeight + 200;
 				}
 			}
 
-		});
-	}
-	burger();
-	
-	// Рассчет высоты формы регистрации и переходы
-	function formBlockHeight() {
-		var 	formBlock = document.querySelectorAll('.form-block'),
-					regSection = document.querySelector('.reg-section'),
-					btnNext = document.querySelector('.reg-bottom__next'),
-					btnBack = document.querySelector('.reg-bottom__back'),
-					regItemUser = document.querySelector('.reg-item--user'),
-					regItemCompany = document.querySelector('.reg-item--company'),
-					maxHeight = 0;
-	
-		for(var i = 0; i < formBlock.length; i++){
+			regSection.style.height = maxHeight + 'px';
 			
-			var elemHeight = formBlock[i].offsetHeight;
-			if(elemHeight > maxHeight){
-				maxHeight = elemHeight + 200;
-			}
+			btnNext.addEventListener('click', () => {
+				regItemUser.classList.remove('active');
+				regItemCompany.classList.add('active');
+			});
+
+			btnBack.addEventListener('click', ()=>{
+				regItemCompany.classList.remove('active');
+				regItemUser.classList.add('active');
+			});
+			
 		}
 
-		regSection.style.height = maxHeight + 'px';
-		
-		btnNext.addEventListener('click', () => {
-			regItemUser.classList.remove('active');
-			regItemCompany.classList.add('active');
-		});
+		formBlockHeight();
 
-		btnBack.addEventListener('click', ()=>{
-			regItemCompany.classList.remove('active');
-			regItemUser.classList.add('active');
-		});
-		
-	}
-
-	formBlockHeight();
 });
